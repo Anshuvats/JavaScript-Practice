188f5581178c8de2650318aff619ece4ea732681
kumara6 <anshu.kumar@hmhco.com>
2025-09-03 20:21:38 +0530

    changes of program1 is completed.

--- Basic-Programming/program1.js
+++ Basic-Programming/program1.js
@@ -1,9 +1,49 @@
-// Write a JavaScript program to display the current day and time in the following format.  
+// Write a JavaScript program to display the current day and time in the following format.
 // Sample Output : Today is : Tuesday.
 // Current time is : 10 PM : 30 : 38
 
 var today = new Date();
 var day = today.getDay();
-var listDay = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var listDay = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-console.log('Today is : '+listDay[day]);
+console.log("Today is : " + listDay[day]);
+
+var hour = today.getHours();
+var min = today.getMinutes();
+var sec = today.getSeconds();
+
+var prepand = hour >= 12 ? "PM" : "AM";
+
+hour = hour >= 12 ? hour - 12 : hour;
+
+if (hour === 0 && prepand === "PM") {
+  if (minute === 0 && second === 0) {
+    hour = 12;
+    prepand = "NOON";
+  } else {
+    hour = 12;
+    prepand = "PM";
+  }
+}
+
+if (hour === 0 && prepand === "AM") {
+  if (minute === 0 && second === 0) {
+    hour = 12;
+    prepand = "MIDNIGHT";
+  } else {
+    hour = 12;
+    prepand = "AM";
+  }
+}
+
+console.log(
+  "Current time is : " + hour + " " + prepand + " :" + min + " :" + sec
+);
